a576185d271af40821db3b990473353f10b97884
EricBarry99 <eric.barry.1@etsmtl.net>
2017-10-06 14:12:52 -0400

    debut de travail sur la classe SeedFill

---
+++ Lab02/src/controller/SeedFill.java
@@ -0,0 +1,12 @@
+package controller;
+
+/*
+  @TODO: Créer la classe seedFill pour remplacer la classeimageLineFiller dans la classe TransformersIndex
+  **cette classe controle les fonctions de fill, elle doit remplacer la classeimageLineFiller dans la classe TransformersIndex
+  **Voir l'attribut theFiller et le constructeur de la classe controller/TransformersIndex.
+  **Voir aussi les méthodes mouseClicked et horizontalLineFill.
+  */
+
+public class SeedFill {
+
+}
